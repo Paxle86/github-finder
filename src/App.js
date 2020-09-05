f694dc4099b1843f2c3279ccc6e75cec4bf0daf2
Thanh <thanh@Thanhs-MacBook-Pro.local>
2020-09-05 14:44:42 +0700

    search user

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import axios from 'axios';
+import Search from './components/users/Search';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  // Search github users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  // Clear github users
+  clearUsers = () => {
+    this.setState({
+      users: [],
+      loading: false,
+    });
+  };
+
+  render() {
+    const { users, loading } = this.state;
+    return (
+      <Fragment>
+        <Navbar />
+        <div className='container'>
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={users} />
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/users/Users.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+
+const Users = ({ loading, users }) => {
+  if (loading) {
+    return Spinner;
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+};
+
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3,1fr)',
+  gridGap: '1rem',
+};
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
+export default Users;
